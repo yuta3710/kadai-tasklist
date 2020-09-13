8e81926427f5bdf5036208972e38c7e62ec6b91a
yuta.ogawa <o.y.3710@gmail.com>
2020-09-13 07:11:45 +0000

    header delete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,22 +8,6 @@
     </head>
 
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">TaskList</a>
-
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                    </ul>
-                </div>
-            </nav>
-        </header>
 
         {{-- ナビゲーションバー --}}
         @include('commons.navbar')
