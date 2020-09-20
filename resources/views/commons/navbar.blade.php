630daec9aca419c0356ef9b06cc53182fc3923be
yuta.ogawa <o.y.3710@gmail.com>
2020-09-20 07:26:47 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,6 +13,12 @@
                 {{-- タスク作成ページへのリンク --}}
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
             </ul>
+            <ul class="nav navbar-nav navbar-right">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li><a href="#">Login</a></li>
+                </ul>
         </div>
     </nav>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', 'TaskController@index');
 
 Route::resource('tasks', 'TaskController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
