bedb41df66e38780b6cbb2cf7748761f7e7e7fd4
yuta.ogawa <o.y.3710@gmail.com>
2020-09-27 09:35:23 +0000

    tasklist edit

--- resources/views/tasks/tasks.blade.php
+++ resources/views/tasks/tasks.blade.php
@@ -4,9 +4,13 @@
             <li class="media mb-3">
                 
                 <div class="media-body">
+                    
                     <div>
+                        {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
+                        {!! link_to_route('users.show', $task->user->id, ['user' => $task->user->id]) !!}
                         <span class="text-muted">posted at {{ $task->created_at }}</span>
                     </div>
+                    
                     <div>
                         {{-- タスク内容 --}}
                         <p class="mb-0">{!! nl2br(e($task->content)) !!}</p>
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{ $user->name }}</h3>
+                </div>
+            </div>
+        </aside>
+        
+        <div class="col-sm-8">
+                {{-- タスク一覧 --}}
+                @include('tasks.tasks')
+            </div>
+    </div>
+@endsection
